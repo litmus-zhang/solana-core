7e03f46e91dd7f935a2d7e542fa9652a770932d8
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-05-10 14:21:30 +0100

    updated buildoor

--- challenges/buildoor/pages/index.tsx
+++ challenges/buildoor/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Box, Stack, Spacer, Center } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
